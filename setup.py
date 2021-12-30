f0d56324589e3945bfe876ec33c0bf3dbacd6b98
Moofbytes <88229513+Moofbytes@users.noreply.github.com>
2021-12-30 15:12:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
+import setuptools
 
+long_description = 'Python package for evaluation of closed-form groundwater flow solutions'
+
+setuptools.setup(
+    name='pygaf',
+    version='0.0.1',
+    author='Tony Smith',
+    author_email='tony@moofbytes.com',
+    description='Installation of testing and development package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Moofbytes/PyGAF',
+    project_urls = {},
+    license='MIT',
+    packages=['pygaf'],
+    install_requires=[],
+)
