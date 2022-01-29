ee632bcdeed68ffc5de718f89901d0fec712a3ef
Moofbytes <88229513+Moofbytes@users.noreply.github.com>
2022-01-29 15:59:37 +0800

    Update grids.py

--- pygaf/grids.py
+++ pygaf/grids.py
@@ -17,8 +17,8 @@ class WellGrid:
         self.well = well
         self.gr = gr
         self.gs = gs
-        max_points = 2500
-        min_points = 25
+        self.max_points = 2500
+        self.min_points = 25
         # Checks
         if self.gr <= 0 or self.gs <= 0:
             print('Error! Grid radius and spacing must be greater than 0.')
@@ -26,20 +26,20 @@ class WellGrid:
         if self.gs >= self.gr:
             print('Error! Grid spacing must be less than grid radius.')
             return
-        # Create setup
+        # Grid setup
         num_points = (1 + 2*self.gr/self.gs)**2
-        if num_points > max_points:
-            self.gs = 2*self.gr/(sqrt(max_points)-1)
+        if num_points > self.max_points:
+            self.gs = 2*self.gr/(sqrt(self.max_points)-1)
             num_points = (1 + 2*self.gr/self.gs)**2
-            print('Notice! the number of grid points exceeds', max_points)
+            print('Notice! the number of grid points exceeds', self.max_points)
             print('The grid spacing is re-set to', round(self.gs, 3))
-        elif num_points < min_points:
+        elif num_points < self.min_points:
             self.gs = self.gr/2
             num_points = (1 + 2*self.gr/self.gs)**2
-            print('Notice! the number of grid points subceeds', min_points)
+            print('Notice! the number of grid points subceeds', self.min_points)
             print('The grid spacing is re-set to', round(self.gs, 3))
-        self.nrow = int(sqrt(num_points))
-        self.ncol = int(sqrt(num_points))
+        #self.nrow = int(sqrt(num_points))
+        #self.ncol = int(sqrt(num_points))
         print('Number of grid points is', round(num_points, 0))
         #self.local_x = list(linspace(-self.gr, self.gr, num_grid_lines))
         #self.local_y = list(linspace(-self.gr, self.gr, num_grid_lines))
@@ -51,30 +51,20 @@ class WellGrid:
             #]
         #print(self.local_x)
         #print(grid_world_x)
+    @property
+    def num_points(self):
+        if self.gr <= 0 or self.gs <= 0:
+            print('Error! Grid radius and spacing must be greater than 0.')
+            return
+        elif self.gs >= self.gr:
+            print('Error! Grid spacing must be less than grid radius.')
+            return
+
 
     @property
     def local_x(self):
         """Local x coordinates of the grid."""
         return list(linspace(-self.gr, self.gr, self.ncol))
 
-        @property
-        def world_x(self):
-            """World x coordinates of the grid."""
-            return [x + self.well.x for x in self.local_x]
-
-        @property
-        def local_y(self):
-            """Local y coordinates of the grid."""
-            return list(linspace(-self.gr, self.gr, self.nrow))
-
-        @property
-        def world_y(self):
-            """World y coordinates of the grid."""
-            return [y + self.well.y for y in self.local_y]
-
-        @property
-        def grid_r(self):
-            """Radius values of grid coordinates."""
-            return [y + self.well.y for y in self.local_y]
 
         return
