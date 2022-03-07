7df30bf093035ed239d579f95a53b1d1cee04634
Moofbytes <88229513+Moofbytes@users.noreply.github.com>
2022-03-07 20:36:13 +0800

    Recovery

--- pygaf/solutions/theis_1935.py
+++ pygaf/solutions/theis_1935.py
@@ -1,28 +1,18 @@
 class TheisWell:
     """Theis (1935) well solution."""
-    is_infinite = True
-    is_finite = False
-    is_homogeneous = True
-    is_heterogeneous = False
-    is_radial = True
-    is_1d = False
-    is_2d = False
-    is_confined = True
-    is_unconfined = False
-
     def __init__(self, aq, w):
         # Checks
-        if not aq.is_radial:
-            print('Error! The solution assumes a radial aquifer.')
+        if not aq.is_2d:
+            print('Error! The solution assumes a 2D aquifer.')
             return
         if not aq.is_infinite:
-            print('Warning! The solution assumes an infinite aquifer.')
+            print('Error! The solution assumes an infinite aquifer.')
             return
         if not aq.is_homogeneous:
-            print('Warning! The solution assumes a homogeneous aquifer.')
+            print('Error! The solution assumes a homogeneous aquifer.')
             return
         if not aq.is_confined:
-            print('Warning! The solution assumes a confined aquifer.')
+            print('Error! The solution assumes a confined aquifer.')
             return
         if not w.is_steady:
             print('Error! The solution assumes a steady state well.')
@@ -43,10 +33,16 @@ class TheisWell:
         print('METHOD REFERENCE')
         print('----------------')
         print(
-        'Theis (1935) - The relation between the lowering of the piezometric' +
-        '\nsurface and the rate and duration of discharge of a well using' +
-        '\nground-water storage.'
+        'Theis C. V. (1935) - The relation between the lowering of the' +
+        '\npiezometric surface and the rate and duration of discharge of a' +
+        '\nwell using ground-water storage.'
         )
+        print('\nConceptual Model:')
+        print('- Infinite, confined, uniform and homogeneous aquifer.')
+        print('- Radial groundwater flow.')
+        print('- Groundwater recharge neglected.')
+        print('- Steady state and fully penetrating well.')
+        print()
 
     def ri(self, t=[1], plot=True, csv='', xlsx=''):
         """
@@ -176,16 +172,20 @@ class TheisWell:
             print('Results exported to:', xlsx)
         return df
 
-    def dd_grid(self, grid, t=1, plot=True, world=False, csv='', xlsx=''):
+    def dd_grid(self, t=1, gr=100, gd=20, plot=True, world=False, csv='',
+    xlsx=''):
         """
-        Compute a rectangular grid of drawdown values.
+        Compute drawdown values on a regular grid.
 
         Arguments:
         ---------
-        grid : object
-            PyGAF WellGrid object
         t : float
             Time to evaluate drawdown (default [1])
+        gr : float
+            Radius defining the extent of the solution grid (default 100)
+        gd : float
+            Grid density defining the number of rows/columns (minimum and
+            maximum constraints are enforced, default 20)
         plot : bool
             Display a drawdown plot (default True)
         world : bool
@@ -200,19 +200,26 @@ class TheisWell:
         -------
         Pandas dataframe
         """
+        from pygaf.grids import WellGrid
         from numpy import pi
         from scipy.special import expn
         import matplotlib.pyplot as plt
         from matplotlib import ticker
-        self.grid = grid
+        self.gr = gr
+        self.gd = gd
+        self.grid = WellGrid(well=self.well, gr=self.gr, gd=self.gd)
         if not world:
             x = self.grid.locx[0]
             y = self.grid.locy[0]
             wx, wy = 0, 0
+            plot_title = 'Displacement at r < ' + str(self.grid.gr) +\
+            ' and t = ' + str(t) + '\n(local coordinates)'
         else:
             x = self.grid.worldx[0]
             y = self.grid.worldy[0]
             wx, wy = self.grid.well.x, self.grid.well.y
+            plot_title = 'Displacement at r < ' + str(self.grid.gr) +\
+            ' and t = ' + str(t) + '\n(world coordinates)'
         drawdown = []
         for i in range(self.grid.grdim): # loop rows
             rr = self.grid.rad_pts[i]
@@ -228,13 +235,11 @@ class TheisWell:
         fig, (ax1, ax2) = plt.subplots(
         2, 1, gridspec_kw={'height_ratios': [4, 1]}, figsize=(6, 7.6)
         )
-        fig.suptitle(
-        'Displacement at r < ' + str(self.grid.gr) + ' and t = ' + str(t)
-        )
+        fig.suptitle(plot_title)
         ax1.contourf(x, y, drawdown, cmap=cm.reversed())
         cs = ax1.contour(x, y, drawdown, linewidths=[0.5], colors=['black'])
         ax1.clabel(cs, inline=1, fontsize=10)
-        ax1.plot(wx, wy, 'o', c='red')
+        ax1.plot(wx, wy, '.', c='red')
         ax1.set_title('Displacement Contours')
         ax1.grid(True)
         ax1.axis('equal')
