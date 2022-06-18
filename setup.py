24c82324197dee47d11922c0a88df104898f4e2d
Moofbytes <88229513+Moofbytes@users.noreply.github.com>
2022-06-18 12:50:26 +0800

    First package build

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 import setuptools
 
+long_description = 'Evaluation and display of analytic groundwater solutions.'
+
 setuptools.setup(
     name='pygaf',
     version='0.0.1',
-    author='Anthony Smith',
+    author='Tony Smith',
     author_email='tony@moofbytes.com',
-    description='Development install and test',
-    long_description='Python Groundwater Analytic Flow',
+    description='Python Groundwater Analytic Flow',
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Moofbytes/PyGAF',
-    project_urls = {},
+    project_urls = {
+        'Documentation': 'https://pygaf.readthedocs.io/en/latest/index.html',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Hydrology',
+        'Development Status :: 3 - Alpha',
+    ],
     license='MIT',
     packages=['pygaf', 'pygaf.solutions'],
 )
