0a8bdf57570757a8dd6723e9f9575ee95c527c77
Moofbytes <88229513+Moofbytes@users.noreply.github.com>
2022-12-12 08:14:33 +0800

    Update

---
+++ docs/mine_flow.rst
@@ -0,0 +1,7 @@
+Mine Pit Flow Solutions
+=======================
+
+.. automodule:: pygaf.solutions.mine_flow
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- pygaf/solutions/__init__.py
+++ pygaf/solutions/__init__.py
@@ -1,9 +1,9 @@
 """
-Analytic solutions for groundwater flow described by author
-and year of publication.
+Analytic solutions for groundwater flow.
 
 """
 
 from .theis_1935 import TheisWell
 from .glover_1960 import GloverRectBasinSteady
 from .steady_flow import Steady1dConfFlow
+from .mine_flow import MineSteadyRadUnconf
