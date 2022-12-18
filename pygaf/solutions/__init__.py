61d9ef77b98694dc0dbf4a08c6746266a9068698
Moofbytes <88229513+Moofbytes@users.noreply.github.com>
2022-12-18 21:08:37 +0800

    Update

--- pygaf/solutions/__init__.py
+++ pygaf/solutions/__init__.py
@@ -6,6 +6,4 @@ Analytic solutions for groundwater flow.
 from .theis_1935 import TheisWell
 from .glover_1960 import GloverRectBasinSteady
 from .steady_flow import Steady1dConfFlow
-from .mine_flow import MineSteadyRadUnconf
-from .mine_flow import MineSteadyRadUnconf2
-from .mine_flow import MineSteadyRadLeaky
+from .mine_flow import *
--- pygaf/solutions/mine_flow.py
+++ pygaf/solutions/mine_flow.py
@@ -19,6 +19,21 @@ class MineSteadyRadUnconf:
         return
 
     @property
+    def R(self):
+        """float : Groundwater recharge rate (units L/T, default 1.0e-4).
+
+        Setter method checks for valid values and triggers an exception if
+        invalid values are specified.
+        """
+        return self._R
+
+    @R.setter
+    def R(self, v):
+        if not (v > 0):
+            raise Exception('Recharge rate (R) must be positive.')
+        self._R = v
+
+    @property
     def rp(self):
         """float : Equivalent radius of mine pit (units L, default 100).
 
@@ -77,18 +92,13 @@ class MineSteadyRadUnconf:
 
     @property
     def dp(self):
-        """float : Drawdown of mine pit water level from initial (pre-mining)
-        water table (units L, default 10)
+        """float : Drawdown of mine pit water level from initial water table
+        (units L, default 10).
         """
         return self.aq.B - self.hp
 
     def info(self):
-        """Print the solution information.
-
-        Returns:
-        Screen printout of solution information.
-
-        """
+        """Print the solution information."""
         print('METHOD REFERENCE')
         print('----------------')
         print('Bouwer H. (1978) - Groundwater Hydrology.')
@@ -156,10 +166,22 @@ class MineSteadyRadUnconf:
             plt.axis([0, None, max(d), min(d)])
             plt.title('Radial Drawdown')
             plt.show()
+        # Export result to csv
+        if csv != '':
+            if csv.split('.') != 'csv':
+                csv = csv + '.csv'
+            df.to_csv(csv)
+            print('Results exported to:', csv)
+        # Export result to Excel
+        if xlsx != '':
+            if xlsx.split('.') != 'xlsx':
+                xlsx = xlsx + '.xlsx'
+            df.to_excel(xlsx, sheet_name='ri')
+            print('Results exported to:', xlsx)
         return df
 
     def draw(self, dw=8):
-        """Display a drawing of the aquifer.
+        """Display the definition diagram.
 
         Args:
             dw (float) : Width of figure (default 8.0).
@@ -179,7 +201,6 @@ class MineSteadyRadUnconf2:
 
     Attributes:
         aq (obj) : Upper aquifer object.
-        R (float) : Groundwater recharge rate (units L/T, default 1.0e-4).
         aq2kx (float) : Horizontal hydraulic conductivity of lower aquifer
             (units L/T, default 1)
         aq2kz (float) : Vertical hydraulic conductivity of lower aquifer
@@ -195,6 +216,21 @@ class MineSteadyRadUnconf2:
         self.aq2kx = 1.0
         self.aq2kz = 1.0
         return
+
+    @property
+    def R(self):
+        """float : Groundwater recharge rate (units L/T, default 1.0e-4).
+
+        Setter method checks for valid values and triggers an exception if
+        invalid values are specified.
+        """
+        return self._R
+
+    @R.setter
+    def R(self, v):
+        if not (v > 0):
+            raise Exception('Recharge rate (R) must be positive.')
+        self._R = v
 
     @property
     def rp(self):
@@ -284,18 +320,13 @@ class MineSteadyRadUnconf2:
 
     @property
     def dp(self):
-        """float : Drawdown of mine pit water level from initial (pre-mining)
-        water table (units L, default 10)
+        """float : Drawdown of mine pit water level from initial water table
+        (units L, default 10).
         """
         return self.aq.B - self.hp
 
     def info(self):
-        """Print the solution information.
-
-        Returns:
-        Screen printout of solution information.
-
-        """
+        """Print the solution information."""
         print('METHOD REFERENCE')
         print('----------------')
         print('Marinelle F. and Niccoli W. (2000) - Simple Analytical\n'
@@ -324,7 +355,11 @@ class MineSteadyRadUnconf2:
         print()
 
     def hr(self, r):
-        """Head at radius r."""
+        """Head at radius r.
+
+        Args:
+            r (float) : radius (units L).
+        """
         from numpy import sqrt
         from numpy import log
         h = sqrt(
@@ -334,7 +369,11 @@ class MineSteadyRadUnconf2:
         return h
 
     def dr(self, r):
-        """Drawdown at radius r."""
+        """Drawdown at radius r.
+
+        Args:
+            r (float) : radius (units L).
+        """
         d = self.aq.B - self.hr(r)
         return d
 
@@ -376,10 +415,22 @@ class MineSteadyRadUnconf2:
             plt.axis([0, None, max(d), min(d)])
             plt.title('Radial Drawdown')
             plt.show()
+        # Export result to csv
+        if csv != '':
+            if csv.split('.') != 'csv':
+                csv = csv + '.csv'
+            df.to_csv(csv)
+            print('Results exported to:', csv)
+        # Export result to Excel
+        if xlsx != '':
+            if xlsx.split('.') != 'xlsx':
+                xlsx = xlsx + '.xlsx'
+            df.to_excel(xlsx, sheet_name='ri')
+            print('Results exported to:', xlsx)
         return df
 
     def draw(self, dw=8):
-        """Display a drawing of the aquifer.
+        """Display the definition diagram.
 
         Args:
             dw (float) : Width of figure (default 8.0).
@@ -442,7 +493,7 @@ class MineSteadyRadLeaky:
 
     @property
     def h0(self):
-        """float : Initial groundwater head (units L, default 90).
+        """float : Initial groundwater head (units L, default 120).
 
         Setter method checks for valid values and triggers an exception if
         invalid values are specified.
@@ -524,19 +575,31 @@ class MineSteadyRadLeaky:
         return r3
 
     def dr(self, r):
-        """Drawdown at radius r."""
+        """Drawdown at radius r.
+
+        Args:
+            r (float) : radius (units L).
+        """
         from numpy import pi
         from scipy.special import k0
         d = k0(r/self.lfac) * self.qp/(2*pi*self.aq.T)
         return d
 
     def hr(self, r):
-        """Head at radius r."""
+        """Head at radius r.
+
+        Args:
+            r (float) : radius (units L).
+        """
         h = self.h0 - self.dr(r)
         return h
 
     def leakr(self, r):
-        """Leakage rate at radius r."""
+        """Leakage rate at radius r.
+
+        Args:
+            r (float) : radius (units L).
+        """
         lr = self.dr(r) * self.Kleak/self.Bleak
         return lr
 
@@ -580,15 +643,22 @@ class MineSteadyRadLeaky:
             plt.axis([0, None, max(d), min(d)])
             plt.title('Radial Drawdown')
             plt.show()
+        # Export result to csv
+        if csv != '':
+            if csv.split('.') != 'csv':
+                csv = csv + '.csv'
+            df.to_csv(csv)
+            print('Results exported to:', csv)
+        # Export result to Excel
+        if xlsx != '':
+            if xlsx.split('.') != 'xlsx':
+                xlsx = xlsx + '.xlsx'
+            df.to_excel(xlsx, sheet_name='ri')
+            print('Results exported to:', xlsx)
         return df
 
     def info(self):
-        """Print the solution information.
-
-        Returns:
-        Screen printout of solution information.
-
-        """
+        """Print the solution information."""
         print('METHOD REFERENCE')
         print('----------------')
         print('Bouwer H. (2000) - Groundwater Hydrology.')
@@ -604,7 +674,7 @@ class MineSteadyRadLeaky:
         print()
 
     def draw(self, dw=8):
-        """Display a drawing of the aquifer.
+        """Display the definition diagram.
 
         Args:
             dw (float) : Width of figure (default 8.0).
@@ -612,4 +682,280 @@ class MineSteadyRadLeaky:
         """
         from pygaf.utils import display_image
         display_image('MineSteadyRadLeaky.png', dw=dw)
+        return
+
+class MineTransRadConf:
+    """Constant, radial, confined flow to a large diameter well.
+
+    Predict transient drawdown for a specified mine pit inflow. The
+    MineTransRadConf class uses the default Aq2dConf aquifer object with
+    initial thickness 100 for the lower confined aquifer.
+
+    Attributes:
+        aq (obj) : Lower aquifer object.
+
+    """
+    from pygaf.aquifers import Aq2dConf
+    def __init__(self):
+        self.aq = self.Aq2dConf(B=100)
+        self.rp = 100
+        self.qp = 1000
+        self.h0 = 110
+        self.dp_targ = 10
+
+    @property
+    def rp(self):
+        """float : Equivalent radius of mine pit (units L, default 100).
+
+        Setter method checks for valid values and triggers an exception if
+        invalid values are specified.
+        """
+        return self._rp
+
+    @rp.setter
+    def rp(self, v):
+        if not (v > 0):
+            raise Exception('Equivalent mine pit radius (rp) must be positive.')
+        self._rp = v
+
+    @property
+    def qp(self):
+        """float : Mine pit inflow rate (units L/T, default 1000).
+
+        Setter method checks for valid values and triggers an exception if
+        invalid values are specified.
+        """
+        return self._qp
+
+    @qp.setter
+    def qp(self, v):
+        if not (v > 0):
+            raise Exception('Mine pit inflow (qp) must be positive.')
+        self._qp = v
+
+    @property
+    def h0(self):
+        """float : Initial groundwater head (units L, default 110).
+
+        Setter method checks for valid values and triggers an exception if
+        invalid values are specified.
+        """
+        return self._h0
+
+    @h0.setter
+    def h0(self, v):
+        if not (v > self.aq.B):
+            raise Exception('Initial water level must be above aquifer top.')
+        self._h0 = v
+
+    @property
+    def dp_targ(self):
+        """float :  Mine pit drawdown target (units L, default 10).
+
+        Setter method checks for valid values and triggers an exception if
+        invalid values are specified.
+        """
+        return self._dp_targ
+
+    @dp_targ.setter
+    def dp_targ(self, v):
+        if not (v > 0):
+            raise Exception('Mine pit drawdown target must be positive.')
+        self._dp_targ = v
+
+    def drt(self, r, t):
+        """Drawdown at specified radius and time.
+
+        Args:
+            r (float) : radius (units L).
+            t (float) : time (units L).
+        """
+        from numpy import pi
+        from scipy.special import expn
+        u = (r**2) * self.aq.S / (4.0 * self.aq.T * t)
+        W = expn(1, u) # Well Function
+        d = W * self.qp / (4.0 * pi * self.aq.T)
+        return d
+
+    def hrt(self, r, t):
+        """Head at specified radius and time.
+
+        Args:
+            r (float) : radius (units L).
+            t (float) : time (units L).
+        """
+        h = self.h0 - self.drt(r, t)
+        return h
+
+    @property
+    def dp_targ_time(self):
+        """Time at which the mine pit drawdown target is reached."""
+        t1 = 1 # intitialise
+        t2 = 1.0e10 # initialise
+        d1 = self.drt(self.rp, t1)
+        d2 = self.drt(self.rp, t2)
+        res = t2 - t1
+        err = 0.01
+        count = 0
+        while abs(res) > err:
+            count = count + 1
+            t3 = t1 + (t2-t1)/2
+            d3 = self.drt(self.rp, t3)
+            if d3 < self.dp_targ:
+                t1 = t3
+            else:
+                t2 = t3
+            res = t2 - t1
+            if count > 1000:
+                raise Exception(('More than 1000 iterations solving dp_targ_time.'))
+        return t3
+
+    def ri(self, t):
+        """Radius of influence at specified time.
+
+        Defined as radius within which change in storage is equal to 95% of
+        total discharge.
+
+        Args:
+            t (float) : time (units L).
+        """
+        from numpy import sqrt
+        from numpy import log
+        e = 0.95
+        r = sqrt(log(1-e) * -4.0 * self.aq.T * t / self.aq.S)
+        return r
+
+    def dp(self, n=25, plot=True, csv='', xlsx=''):
+        """Transient drawdown of mine pit water level.
+
+        Evaluate transient drawdown of the water level in the mine pit.
+        Results are returned in a Pandas dataframe. A drawdown graph is
+        displayed as default and can be suppressed by setting plot=False.
+
+        Args:
+            n (int) : Number of radial values for evaluating drawdown (default 25).
+            plot (bool) : Display a plot of results (default True).
+            csv (str) : Full filepath for export of results to csv file;
+                results are exported if the string is not empty (default '').
+            xlsx (str) : Full filepath for export of result to xlsx file;
+                results are exported if the string is not empty (default '').
+
+        Returns:
+            Results in a pandas dataframe.
+        """
+        from numpy import linspace
+        import pandas
+        t = linspace(self.dp_targ_time/n, self.dp_targ_time, n)
+        d = [self.drt(self.rp, i) for i in t]
+        h = [self.hrt(self.rp, i) for i in t]
+        r = [self.ri(i) for i in t]
+        df = pandas.DataFrame()
+        df['time'] = t
+        df['drawdown'] = d
+        df['head'] = h
+        df['ri'] = r
+        # Plot results
+        if plot:
+            import matplotlib.pyplot as plt
+            df.plot(
+                x='time', y='drawdown', grid=True, marker='.', lw=3, alpha=0.5,
+                legend=False, ylabel='drawdown'
+                )
+            plt.axis([0, None, max(d), min(d)])
+            plt.title('Time Drawdown at Mine Pit')
+            plt.show()
+        # Export result to csv
+        if csv != '':
+            if csv.split('.') != 'csv':
+                csv = csv + '.csv'
+            df.to_csv(csv)
+            print('Results exported to:', csv)
+        # Export result to Excel
+        if xlsx != '':
+            if xlsx.split('.') != 'xlsx':
+                xlsx = xlsx + '.xlsx'
+            df.to_excel(xlsx, sheet_name='ri')
+            print('Results exported to:', xlsx)
+        return df
+
+    def dd(self, t, n=25, plot=True, csv='', xlsx=''):
+        """Evaluate radial drawdown at specified time.
+
+        Results are returned in a Pandas dataframe. A drawdown graph is
+        displayed as default and can be suppressed by setting plot=False.
+
+        Args:
+            t (float) : Time for evaluating drawdown (units T).
+            n (int) : Number of radial values for evaluating drawdown (default 25).
+            plot (bool) : Display a plot of results (default True).
+            csv (str) : Full filepath for export of results to csv file;
+                results are exported if the string is not empty (default '').
+            xlsx (str) : Full filepath for export of result to xlsx file;
+                results are exported if the string is not empty (default '').
+
+        Returns:
+            Results in a pandas dataframe.
+
+        """
+        from numpy import linspace
+        import pandas
+        r = linspace(self.rp, self.ri(t), n)
+        d = [self.drt(i, t) for i in r]
+        h = [self.hrt(i, t) for i in r]
+        df = pandas.DataFrame()
+        df['radius'] = r
+        df['drawdown'] = d
+        df['head'] = h
+        # Plot results
+        if plot:
+            import matplotlib.pyplot as plt
+            df.plot(
+                x='radius', y='drawdown', grid=True, marker='.', lw=3, alpha=0.5,
+                legend=False, ylabel='drawdown'
+                )
+            plt.axis([0, None, max(d), min(d)])
+            plt.title('Radial Drawdown at Time: ' + str(round(t, 0)))
+            plt.show()
+        # Export result to csv
+        if csv != '':
+            if csv.split('.') != 'csv':
+                csv = csv + '.csv'
+            df.to_csv(csv)
+            print('Results exported to:', csv)
+        # Export result to Excel
+        if xlsx != '':
+            if xlsx.split('.') != 'xlsx':
+                xlsx = xlsx + '.xlsx'
+            df.to_excel(xlsx, sheet_name='ri')
+            print('Results exported to:', xlsx)
+        return df
+
+    def info(self):
+        """Print the solution information."""
+        print('METHOD REFERENCE')
+        print('----------------')
+        print('Leczfalvy S. (1982) - Simplified Mathematical Models for the\n'
+        'Calculation of Dewatering.')
+        print('\nAquifer Conceptual Model:')
+        print('- Infinite, confined and homogeneous aquifer.')
+        print('- Mine pit is approximated by a cylinder.')
+        print('- Dewatering via an imaginary pumping well with radius rp.')
+        print('- Mine pit fully penetrates aquifer.')
+        print('- Transient, horizontal, axially symmetric and confined flow.')
+        print('- Horizontal pre-mining groundwater head.')
+        print('\nNotes:')
+        print('- Inflow to the mine pit is constant and drawdown is transient.')
+        print('- A steady state is not reached because all discharge is from\n'
+        '  aquifer storage.')
+        print()
+
+    def draw(self, dw=8):
+        """Display the drawing definition.
+
+        Args:
+            dw (float) : Width of figure (default 8.0).
+
+        """
+        from pygaf.utils import display_image
+        display_image('MineTransRadConf.png', dw=dw)
         return
