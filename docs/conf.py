75d6db604623d44005c1d67a3732adec6eac29eb
Moofbytes <88229513+Moofbytes@users.noreply.github.com>
2024-08-14 20:24:50 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('./solutions'))
+sys.path.insert(0, os.path.abspath('../pygaf/solutions'))
+sys.path.insert(0, os.path.abspath('../pygaf'))
 
 
 # -- Project information -----------------------------------------------------
